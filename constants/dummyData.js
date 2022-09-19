059d585f7eb8f3b89b2d75eab12c04d33c229f1e
ankitmandiratta <87859899+ankitmandiratta@users.noreply.github.com>
2022-09-19 13:04:25 +0530

    19.09 13.00

---
+++ constants/dummyData.js
@@ -0,0 +1,102 @@
+const categories = [
+    {
+        id: 1,
+        name: "Iphone",
+        icon: icons.burger
+    },
+    {
+        id: 2,
+        name: "Samsung",
+        icon: icons.cherry
+    },
+	    {
+        id: 3,
+        name: "OnePlus",
+        icon: icons.cherry
+    },
+    {
+        id: 4,
+        name: "Mi",
+        icon: icons.rice
+    },
+	    {
+        id: 5,
+        name: "Realme",
+        icon: icons.rice
+    },
+	    {
+        id: 6,
+        name: "Vivo",
+        icon: icons.rice
+    }	
+]
+
+const Phone1 = {
+    id: 1,
+    company: "Samsung",
+    model: "Galaxy M32",
+    sim: [1, 2, 3],
+    network: "4G",
+    screen_size: "78*78",
+    
+    platform:"Android 11",
+    selfie_camera:1,
+    main_camera:32,
+    ram:16,
+    memory_internal:32,
+    memoryexternal:null,
+    battery:4000,
+    amount1:8000, 
+    amount2:6290,
+    // image1: require("../assets/dummyData/veg_biryani.png"),
+    // image2: require("../assets/dummyData/veg_biryani.png"),
+    // image3: require("../assets/dummyData/veg_biryani.png"),
+    // image4: require("../assets/dummyData/veg_biryani.png")
+}
+
+const Phone2 = {
+    id: 2,
+    company: "Samsung",
+    model: "Galaxy M32",
+    sim: [1, 2, 3],
+    network: "4G",
+    screen_size: "78*78",
+    
+    platform:"Android 11",
+    selfie_camera:1,
+    main_camera:32,
+    ram:16,
+    memory_internal:32,
+    memoryexternal:null,
+    battery:4000,
+    amount1:8000, 
+    amount2:6290,
+    // image1: require("../assets/dummyData/veg_biryani.png"),
+    // image2: require("../assets/dummyData/veg_biryani.png"),
+    // image3: require("../assets/dummyData/veg_biryani.png"),
+    // image4: require("../assets/dummyData/veg_biryani.png")
+}
+const Phone3 = {
+    id: 3,
+    company: "Samsung",
+    model: "Galaxy M32",
+    sim: [1, 2, 3],
+    network: "4G",
+    screen_size: "78*78",
+    
+    platform:"Android 11",
+    selfie_camera:1,
+    main_camera:32,
+    ram:16,
+    memory_internal:32,
+    memoryexternal:null,
+    battery:4000,
+    amount1:8000, 
+    amount2:6290,
+    // image1: require("../assets/dummyData/veg_biryani.png"),
+    // image2: require("../assets/dummyData/veg_biryani.png"),
+    // image3: require("../assets/dummyData/veg_biryani.png"),
+    // image4: require("../assets/dummyData/veg_biryani.png")
+}
+
+export default {categories,Phone1,Phone2,Phone3}
